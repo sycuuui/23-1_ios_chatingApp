40ef960b524b85ad9bc2508ca2e1a24e0a095fdb
sycuuui <102959791+sycuuui@users.noreply.github.com>
2023-06-15 15:54:17 +0900

    Initial Commit

---
+++ myProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myProject
+//
+//  Created by 이서연 on 2023/06/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
